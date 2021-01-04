5904a4eb78aa686a72db32e5d00f2943cce08dcb
Artem Manko <mankoartem2@gmail.com>
2021-01-04 20:04:10 +0300

    04.01.2021

--- src/main/resources/db/migration/V2__Schedule_DB.sql
+++ src/main/resources/db/migration/V2__Schedule_DB.sql
@@ -4,9 +4,17 @@ schedule_level varchar(255),
 start_end_time varchar(255) not null,
 primary key (id));
 
+create table users_schedule_workouts (
+user_id int8 not null,
+schedule_id int8 not null);
+
 create table schedule_workout_days (
 schedule_workout_id int8 not null,
 schedule_day varchar(255));
+
+alter table if exists schedule_workout_days
+add constraint FKf5kenlsdpcva8f94orlal5gry
+foreign key (schedule_workout_id) references schedule_workout;
 
 alter table if exists users_schedule_workouts
 add constraint FK72ydo7ipyy04au22synoygsal
@@ -16,6 +24,4 @@ alter table if exists users_schedule_workouts
 add constraint FK67edehvpy5r9bwp5dfpx33f3n
 foreign key (user_id) references users;
 
-alter table if exists schedule_workout_days
-add constraint FKf5kenlsdpcva8f94orlal5gry
-foreign key (schedule_workout_id) references schedule_workout;
+
